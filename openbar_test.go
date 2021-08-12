208a11c4411b5dc949fcfebe05d9dbc765a352e4
mzanibelli <mzanibelli@gmail.com>
2021-08-12 21:42:53 +0200

    fix: do not sleep in tests

--- openbar_test.go
+++ openbar_test.go
@@ -13,9 +13,9 @@ import (
 )
 
 func TestOpenBar(t *testing.T) {
-	wg := new(sync.WaitGroup)
-
-	wg.Add(1)
+	w1, w2 := new(sync.WaitGroup), new(sync.WaitGroup)
+	w1.Add(1)
+	w2.Add(1)
 
 	stdout := bytes.NewBuffer(nil)
 	stderr := bytes.NewBuffer(nil)
@@ -23,12 +23,15 @@ func TestOpenBar(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// We need a sync.Once here to wait for at least one screen update.
+	var once sync.Once
 	module := openbar.ModuleFunc(func() (string, error) {
+		defer once.Do(w1.Done)
 		return "hello", nil
 	})
 
 	go func() {
-		defer wg.Done()
+		defer w2.Done()
 		if err := openbar.Run(
 			ctx,
 			openbar.WithOutput(stdout),
@@ -40,11 +43,9 @@ func TestOpenBar(t *testing.T) {
 		}
 	}()
 
-	time.Sleep(1 * time.Millisecond)
-
-	cancel()
-
-	wg.Wait()
+	w1.Wait() // Wait for update.
+	cancel()  // Stop.
+	w2.Wait() // Wait for shutdown.
 
 	if stderr.String() != "" {
 		t.Error(stderr.String())
